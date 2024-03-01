817dd02b6e2eb8b6b682c58d2f425ebaf38730bd
Son Anthony Estomagulang <120387951+r111n@users.noreply.github.com>
2024-03-02 00:56:58 +0800

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -8,19 +8,20 @@ import {
 import './index.css'
 import Login from './routes/user/Login.jsx';
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "/login",
-    element: <Login />
-  }
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <App />,
+//   },
+//   {
+//     path: "/login",
+//     element: <Login />
+//   }
+// ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    {/* <RouterProvider router={router} /> */}
+    <App />
   </React.StrictMode>,
 )
